d7cbcd729c6aca3cf5e45d3cb310925db8ff50bf
Sakana <admin@yangrucheng.top>
2025-03-23 13:28:11 +0800

    feat: host

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,20 +1,21 @@
 #! /bin/bash
 
-URL="https://cdn.jsdelivr.net/gh/YangRucheng/Config-Backup@main/resource"
+HOST="https://jsdelivr.proxy.yangrucheng.top"
+# HOST="https://cdn.jsdelivr.net"
 
-wget --tries=10 -O ~/.bashrc "$URL/.bashrc"
+URL="https://$HOST/gh/YangRucheng/Config-Backup@main/resource"
+
 wget --tries=10 -O ~/.npmrc "$URL/.npmrc"
+wget --tries=10 -O ~/.bashrc "$URL/.bashrc"
 wget --tries=10 -O ~/.tmux.conf "$URL/.tmux.conf"
 wget --tries=10 -O ~/.bash_profile "$URL/.bash_profile"
+
+mkdir -p /etc/docker && wget --tries=10 -O /etc/docker/daemon.json "$URL/daemon.json"
+mkdir -p ~/.m2 && wget --tries=10 -O ~/.m2/settings.xml "$URL/.m2/settings.xml"
+mkdir -p ~/.ssh && chmod 600 ~/.ssh && wget --tries=10 -O ~/.ssh/authorized_keys "$URL/.ssh/authorized_keys"
+
 source ~/.bashrc && echo "Host" > /etc/hostname && hostname Host
-
-
-mkdir -p ~/.ssh && chmod 600 ~/.ssh
-wget --tries=10 -O ~/.ssh/authorized_keys "$URL/.ssh/authorized_keys"
 sed -i "s/#PermitRootLogin prohibit-password/PermitRootLogin yes/" /etc/ssh/sshd_config
 sed -i "s/#PubkeyAuthentication yes/PubkeyAuthentication yes/" /etc/ssh/sshd_config
-
-mkdir -p /etc/docker
-wget --tries=10 -O /etc/docker/daemon.json "$URL/daemon.json"
 
 echo "Success! 执行成功！"
